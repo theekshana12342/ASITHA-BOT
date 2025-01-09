d8e54a8a7117c6a4abed59a59de7aa5b6dda2749
ASITHA-MD <Kavishanrashmika3@gmail.com>
2025-01-09 08:01:24 +0530

    ASITHA-MD

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=Kyph3aoZ#FEff1DBdJ1UMHhWoQJzPzYYT_w0pZ0wrobZ0gLGRgm0' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
